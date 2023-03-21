57232ad5a9d0b5aee8f182bc1db551c457aa9822
Njezi <jbm.pl34@gmail.com>
2023-03-21 14:29:01 +0100

    Basic if statements and functions

---
+++ menu.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <memoryapi.h>
+#include <winable.h>
+using namespace std;
+
+int main()
+{
+    string user;
+
+    cout << "What would you want to enable?\nGod Mode (G)\nRun Speed (R)\nSuicide (S)\n";
+    cin >> user;
+    cout << user << "\n";
+
+
+    //hey this is my first time making a gta 5 menu in c++ so if the if statements make ur head hurt please tell me how to fix it lol
+
+    if (user == "G" || user == "g")
+    {
+        cout << "God Mode\n";
+    }
+    else if (user == "R" || user == "r")
+    {
+        cout << "Run Speed\n";
+    }
+    else if (user == "S" || user == "s")
+    {
+        cout << "Suicide\n";
+    }
+}
